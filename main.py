8d484c1ce5c0911cda023cd5edd97cc9aa8007cf
rayanearaujoc <rayane.araujo.cunha@gmail.com>
2025-06-12 11:29:43 -0300

    adição do dataset

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ import pandas as pd
 
 df = pd.read_csv("creditcard.csv") 
 relatorio = sv.analyze(df)
-relatorio.show_html("relatorio_fraude.html")
+relatorio.show_html("relatorio_fraude.html")  
+ 
